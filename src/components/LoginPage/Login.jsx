f4688b8de19be248a69d2770d09e621dacc92429
hyoyoung-OH <lisa4397@gmail.com>
2022-04-01 20:26:42 +0900

    회원가입, 로그인 html 작업

--- src/components/LoginPage/Login.jsx
+++ src/components/LoginPage/Login.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
     <>
-      로그인 페이지 입니다
-      <Link to="/memberregister">
+      <h1>로그인</h1>
+      <form>
+        <input name='username' placeholder='아이디' />
+        <input name='password' placeholder='비밀번호' type='password' />
+        <button>로그인</button>
+      </form>
+
+      <Link to='/member-register'>
         <button>개인 회원가입</button>
       </Link>
-      <Link to="/ownerregister">
+      <Link to='/owner-register'>
         <button>사장님 회원가입</button>
       </Link>
     </>
