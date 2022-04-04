5bfa7a4ff8d69baa5aa0df3353938088119b3a9f
hyoyoungOH <lisa4397@gmail.com>
2022-04-05 00:47:23 +0900

    cart 마크업

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import classes from './Cart.module.css';
 
 const Cart = () => {
-  return <div>Cart 브랜치테스트</div>;
+  return (
+    <div className={classes.Cart}>
+      <section className='title'>
+        <h1>Cart</h1>
+      </section>
+      <section className='storeName'>
+        <h2>Foodtruck Name(동적으로 받아와야함)</h2>
+      </section>
+      <section className='menuBar'>
+        <span>메뉴명</span>
+        <span>수량</span>
+        <span>가격</span>
+      </section>
+      <section className='itemList'></section>
+      <section className='totalPrice'>
+        <h2>Total Price :</h2>
+        <h2>금액계산(동적)</h2>
+      </section>
+      <section className='orderBtn'>
+        {/* 버튼 누르면 주문되었습니다 팝업 */}
+        <button type='button' class='btn btn-outline-secondary'>
+          order
+        </button>
+      </section>
+    </div>
+  );
 };
 
 export default Cart;
