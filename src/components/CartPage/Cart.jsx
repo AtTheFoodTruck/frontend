d4002ef359ba10eddeb50d3c033e1cd93584245f
hyoyoungOH <lisa4397@gmail.com>
2022-04-04 22:27:35 +0900

    브랜치 테스트

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Cart = () => {
-    return (
-        <div>
-            Cart 페이지 입니다.
-        </div>
-    );
+  return <div>Cart 브랜치테스트</div>;
 };
 
 export default Cart;
