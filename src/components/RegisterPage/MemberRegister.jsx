5687c87063b2580e617907bdaf162ef36e0e0772
hyoyoungOH <lisa4397@gmail.com>
2022-04-06 01:39:17 +0900

    회원가입 퍼블리싱

--- src/components/RegisterPage/MemberRegister.jsx
+++ src/components/RegisterPage/MemberRegister.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 // import { useRef } from 'react';
 
 // //ref로 사용자 입력 가져옴, onchange로 변경해볼까 고민중..
@@ -18,28 +17,87 @@ import React from 'react';
 
 const MemberRegister = () => {
   return (
-    <section>
-      <h1>개인 회원가입</h1>
-      <form>
-        <label htmlFor='email'>이메일</label>
-        <input type='email' id='email' />
-        <button>중복확인</button>
-        <label htmlFor='nickname'>닉네임</label>
-        <input type='nickname' id='nickname' />
-        <button>중복확인</button>
-        <label htmlFor='password'>비밀번호</label>
-        <input type='password' id='password' />
-        <label htmlFor='passwordConfirm'>비밀번호 확인</label>
-        <input
-          type='password'
-          id='passwordConfirm'
-          placeholder='비밀번호 확인'
-        />
-        <label htmlFor='phonenumber'>전화번호</label>
-        <input type='phonenumber' id='phonenumber' required />
-        <button>회원가입</button>
-      </form>
-    </section>
+    <>
+      <section className='container mt-5 p-5'>
+        <h1 className='text-center'>Join</h1>
+        <div className='row'>
+          <div className='col'></div>
+          <div className='col'>
+            <div className='container mt-sm-5'>
+              <div className='input-group mb-3'>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='이메일'
+                  aria-label="Recipient's username"
+                  aria-describedby='button-addon2'
+                />
+                <button
+                  className='btn btn-primary ms-3'
+                  type='button'
+                  id='button-addon2'
+                >
+                  중복확인
+                </button>
+              </div>
+              <div className='input-group mb-3'>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='닉네임'
+                  aria-label="Recipient's username"
+                  aria-describedby='button-addon2'
+                />
+                <button
+                  className='btn btn-primary ms-3'
+                  type='button'
+                  id='button-addon2'
+                >
+                  중복확인
+                </button>
+              </div>
+              <div className='input-group mb-3 w-75'>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='비밀번호'
+                  aria-label="Recipient's username"
+                  aria-describedby='button-addon2'
+                />
+              </div>
+              <div className='input-group mb-3 w-75'>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='비밀번호 확인'
+                  aria-label="Recipient's username"
+                  aria-describedby='button-addon2'
+                />
+              </div>
+              <div className='input-group mb-3 w-75'>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='전화번호'
+                  aria-label="Recipient's username"
+                  aria-describedby='button-addon2'
+                />
+              </div>
+            </div>
+          </div>
+          <div className='col'></div>
+        </div>
+      </section>
+      <section className='container mt-1'>
+        <div className='row text-center'>
+          <div className='col'>
+            <button type='button' class='btn btn-outline-secondary'>
+              가입하기
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 //   };
