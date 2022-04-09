249e760064a0ee10c01941e72edee5033865c6db
hyoyoungOH <lisa4397@gmail.com>
2022-04-09 19:27:40 +0900

    장바구니 - 정렬중

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -1,50 +1,46 @@
-import React from "react";
-import classes from "./Cart.module.css";
-import CartItemList from "./CartItemList";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import React from 'react';
+import classes from './Cart.module.css';
+import CartItemList from './CartItemList';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 const Cart = () => {
   return (
     <Container>
-      <section className="title">
-        <h1>Cart</h1>
-      </section>
-      <section className="storeName">
-        <h2>Foodtruck Name</h2>
-      </section>
-      <section className="menuBar">
-        <span>메뉴명</span>
-        <span>수량</span>
-        <span>가격</span>
-      </section>
-      <section className="itemList">
-        <CartItemList />
-      </section>
-      <section className="totalPrice">
-        <h2>Total Price :</h2>
-        <h2>금액계산(동적)</h2>
-      </section>
-      <section className="orderBtn">
-        {/* 버튼 누르면 주문되었습니다 팝업 */}
-        <button type="button" class="btn btn-outline-secondary">
-          order
-        </button>
-      </section>
-
       <h1>Cart</h1>
-      <Row className="StoreName">
+      <Row className='StoreName d-flex justify-content-start'>
         <h4>Store Name</h4>
       </Row>
-      <Row>
-        <h5>수량</h5>
-        <h5>가격</h5>
-        <h5>메뉴</h5>
+      <Row className='d-flex justify-content-evenly'>
+        <Col lg={5}></Col>
+        <Col className='d-flex justify-content-center p-0 ms-5 me-4'>
+          <h5>Menu</h5>
+        </Col>
+        <Col className='d-flex justify-content-center p-0 me'>
+          <h5>Amout</h5>
+        </Col>
+        <Col className='d-flex justify-content-center p-0 me-3'>
+          <h5>Price</h5>
+        </Col>
       </Row>
       <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
+        <Col lg={6} className='d-flex justify-content-start'>
+          <Card style={{ width: '8rem', height: '8rem' }}>
+            <Card.Img
+              variant='top'
+              src='https://mp-seoul-image-production-s3.mangoplate.com/999285_1575800181007324.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80'
+            />
+          </Card>
+        </Col>
+        <Col className='d-flex align-items-center'>카레라이스</Col>
+        <Col className='d-flex align-items-center ms-4'>
+          <Button variant='outline-secondary'>+</Button>{' '}
+          <Button variant='outline-secondary disabled'>0</Button>{' '}
+          <Button variant='outline-secondary'>-</Button>{' '}
+        </Col>
+        <Col className='d-flex align-items-center'>12,000</Col>
+        <Row className='text-end'>
+          <h4>총 금액 : 20,000</h4>
+        </Row>
       </Row>
     </Container>
   );
