ea9c59fb628e277adb9712b6813b4aeef2dc7978
Ojea <rnjsdhwo1325@gmail.com>
2022-04-09 19:20:03 +0900

    main퍼블리싱완성

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,92 +1,35 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import DropDown from '../MyPage/DropDown';
-import styles from './Header.module.css';
-import { Nav, NavDropdown } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import HeaderInput from "./HeaderInput";
+import HeaderNav from "./HeaderNav";
 
+const NavH = styled.nav`
+  display: flex;
+  margin-left: 15px;
+  margin-top: 10px;
+  width: 99%;
+  height: 90px;
+  font-size: 25px;
+  position: fixed;
+  left: 0;
+  right: 0;
+`;
+const StyledLink = styled(Link)`
+  font-size: 30px;
+`;
 const Header = () => {
-  // const [dropdown, setDropdown] = useState(false);
-
-  // const onMouseEnter = () => {
-  //   if (window.innerWidth < 960) {
-  //     setDropdown(false);
-  //     console.log('false' + window.innerWidth);
-  //   } else {
-  //     setDropdown(true);
-  //     console.log('true' + window.innerWidth);
-  //   }
-  // };
-
-  // const onMouseLeave = () => {
-  //   if (window.innerWidth < 960) {
-  //     setDropdown(false);
-  //   } else {
-  //     setDropdown(false);
-  //     console.log('false' + window.innerWidth);
-  //   }
-  // };
-
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-      <div className='container-fluid'>
-        <Link to='/' className='navbar-brand'>
-          Foodtruck Around Me
-        </Link>
-        <button
-          class='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarColor03'
-          aria-controls='navbarColor03'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-
-        {/* 검색창 */}
-        <form className='d-flex'>
-          <input
-            className='form-control me-sm-3'
-            type='text'
-            placeholder='메뉴명을 입력해주세요!'
-          />
-          <button class='btn btn-secondary my-2 my-sm-0' type='submit'>
-            Search
-          </button>
-        </form>
-
-        <div className='collapse navbar-collapse' id='navbarColor03'>
-          <ul className='navbar-nav me-auto'>
-            <li className='nav-item'>
-              <Link to='/' className='nav-link'>
-                Home
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/store' className='nav-link'>
-                Store
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/cart' className='nav-link'>
-                Cart
-              </Link>
-            </li>
-            <NavDropdown title='Mypage' id='nav-dropdown'>
-              <NavDropdown.Item eventKey='4.1'>정보수정</NavDropdown.Item>
-              <NavDropdown.Item eventKey='4.2'>주문내역</NavDropdown.Item>
-              <NavDropdown.Item eventKey='4.3'>리뷰관리</NavDropdown.Item>
-            </NavDropdown>
-            <li className='nav-item'>
-              <Link to='/login' className='nav-link'>
-                Login
-              </Link>
-            </li>
-          </ul>
+    <>
+      <NavH className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid ">
+          <StyledLink className="navbar-brand h4" to="/">
+            Foodtruck Around Me
+          </StyledLink>
+          <HeaderInput />
+          <HeaderNav />
         </div>
-      </div>
-    </nav>
+      </NavH>
+    </>
   );
 };
 
---
+++ src/components/Layout/HeaderNav.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import DropDown from "../MyPage/DropDown";
+
+const NavR = styled.div`
+  display: flex;
+  justify-content: end;
+  margin-right: 90px;
+
+  //네비게이션 메뉴바 오른쪽 정렬
+`;
+
+const HeaderNav = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+      console.log("false" + window.innerWidth);
+    } else {
+      setDropdown(true);
+      console.log("true" + window.innerWidth);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+      console.log("false" + window.innerWidth);
+    }
+  };
+
+  return (
+    <NavR className="collapse navbar-collapse " id="navbarColor03">
+      <ul className="navbar-nav ">
+        <li className="nav-item">
+          <Link className="nav-link active" to="/">
+            Home
+            <span className="visually-hidden">(current)</span>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/store">
+            store
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/cart">
+            cart
+          </Link>
+        </li>
+        <li
+          className="nav-item dropdown"
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        >
+          <Link className="nav-link dropdown-toggle" to="/userinfo-config">
+            mypage
+          </Link>
+          {dropdown && <DropDown />}
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            login
+          </Link>
+        </li>
+      </ul>
+    </NavR>
+  );
+};
+
+export default HeaderNav;
