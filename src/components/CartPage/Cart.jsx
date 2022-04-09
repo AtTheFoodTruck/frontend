b6eea659496c4ec7e8eea5b913373f33c7055cb5
hyoyoungOH <lisa4397@gmail.com>
2022-04-09 20:40:09 +0900

    장바구니 퍼블 완료

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import classes from './Cart.module.css';
-import CartItemList from './CartItemList';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 const Cart = () => {
   return (
-    <Container>
-      <h1>Cart</h1>
-      <Row className='StoreName d-flex justify-content-start'>
+    <Container className='mt-5'>
+      <h1 className='text-center'>Cart</h1>
+      <Row className='StoreName d-flex justify-content-start mt-5'>
         <h4>Store Name</h4>
       </Row>
-      <Row className='d-flex justify-content-evenly'>
+
+      <Row className='d-flex justify-content-evenly mt-5'>
         <Col lg={5}></Col>
         <Col className='d-flex justify-content-center p-0 ms-5 me-4'>
           <h5>Menu</h5>
         </Col>
-        <Col className='d-flex justify-content-center p-0 me'>
+        <Col className='d-flex justify-content-center p-0'>
           <h5>Amout</h5>
         </Col>
         <Col className='d-flex justify-content-center p-0 me-3'>
           <h5>Price</h5>
         </Col>
       </Row>
+      <hr />
       <Row>
         <Col lg={6} className='d-flex justify-content-start'>
           <Card style={{ width: '8rem', height: '8rem' }}>
@@ -31,17 +31,23 @@ const Cart = () => {
             />
           </Card>
         </Col>
-        <Col className='d-flex align-items-center'>카레라이스</Col>
-        <Col className='d-flex align-items-center ms-4'>
+        <Col className='d-flex align-items-center me-5'>카레라이스</Col>
+        <Col className='d-flex align-items-center ms-3 me-5'>
           <Button variant='outline-secondary'>+</Button>{' '}
           <Button variant='outline-secondary disabled'>0</Button>{' '}
           <Button variant='outline-secondary'>-</Button>{' '}
         </Col>
-        <Col className='d-flex align-items-center'>12,000</Col>
-        <Row className='text-end'>
+        <Col className='d-flex align-items-center ms-5'>12,000</Col>
+        <hr className='mt-5' />
+        <Row className='text-end mt-5'>
           <h4>총 금액 : 20,000</h4>
         </Row>
       </Row>
+      <Container className='text-center'>
+        <Button className='mt-5' size='lg' variant='outline-secondary'>
+          주문하기
+        </Button>{' '}
+      </Container>
     </Container>
   );
 };
