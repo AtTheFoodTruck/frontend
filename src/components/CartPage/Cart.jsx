d743d456b0b058a00cdccb7f3e1197e5abef1aac
hyoyoungOH <lisa4397@gmail.com>
2022-04-09 17:44:28 +0900

    장바구니 퍼블리싱 미완성- 재정렬필요

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -1,35 +1,52 @@
-import React from 'react';
-import classes from './Cart.module.css';
-import CartItemList from './CartItemList';
+import React from "react";
+import classes from "./Cart.module.css";
+import CartItemList from "./CartItemList";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const Cart = () => {
   return (
-    <div className={classes.Cart}>
-      <section className='title'>
+    <Container>
+      <section className="title">
         <h1>Cart</h1>
       </section>
-      <section className='storeName'>
-        <h2>Foodtruck Name(동적으로 받아와야함)</h2>
+      <section className="storeName">
+        <h2>Foodtruck Name</h2>
       </section>
-      <section className='menuBar'>
+      <section className="menuBar">
         <span>메뉴명</span>
         <span>수량</span>
         <span>가격</span>
       </section>
-      <section className='itemList'>
+      <section className="itemList">
         <CartItemList />
       </section>
-      <section className='totalPrice'>
+      <section className="totalPrice">
         <h2>Total Price :</h2>
         <h2>금액계산(동적)</h2>
       </section>
-      <section className='orderBtn'>
+      <section className="orderBtn">
         {/* 버튼 누르면 주문되었습니다 팝업 */}
-        <button type='button' class='btn btn-outline-secondary'>
+        <button type="button" class="btn btn-outline-secondary">
           order
         </button>
       </section>
-    </div>
+
+      <h1>Cart</h1>
+      <Row className="StoreName">
+        <h4>Store Name</h4>
+      </Row>
+      <Row>
+        <h5>수량</h5>
+        <h5>가격</h5>
+        <h5>메뉴</h5>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </Container>
   );
 };
 
