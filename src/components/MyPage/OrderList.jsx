7fe9ad1e5b3b4baf4454796d300c792ebdfe1419
hyoyoungOH <lisa4397@gmail.com>
2022-04-10 19:00:06 +0900

    리뷰작성페이지-develop merge 완료 및 퍼블 완료

--- src/components/MyPage/OrderList.jsx
+++ src/components/MyPage/OrderList.jsx
@@ -1,14 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const OrderListWrapper = styled.div`
+  position: absolute;
+  align-items: center;
+  width: 40%;
+  top: 20%;
+  left: 30%;
+`;
 
 const OrderList = () => {
   return (
-    <>
+    <OrderListWrapper>
       <div>주문 내역 페이지 입니다.</div>
-      <Link to="/reviewwriting">
+      <Link to='/review-writing'>
         <button>리뷰 쓰기</button>
       </Link>
-    </>
+    </OrderListWrapper>
   );
 };
 
--- src/components/MyPage/ReviewWriting.jsx
+++ src/components/MyPage/ReviewWriting.jsx
@@ -1,12 +1,74 @@
 import React from 'react';
+import styled from 'styled-components';
+import { AiFillStar } from 'react-icons/ai';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+const ReviewWritinghWrapper = styled.div`
+  position: absolute;
+  align-items: center;
+  width: 40%;
+  top: 20%;
+  left: 30%;
+`;
 
 const ReviewWriting = () => {
-    return (
-        <div>
-            리뷰 등록 페이지 입니다.
-            
-        </div>
-    );
+  return (
+    <ReviewWritinghWrapper>
+      <Container className='text-center'>
+        <h1>리뷰 작성하기</h1>
+        <Row className='mt-5'>
+          <Col>
+            <h3>매장명</h3>
+          </Col>
+          <Col>
+            <h3>메뉴명</h3>
+          </Col>
+        </Row>
+        <hr />
+        <Row className='mb-5'>
+          <Col>동대문엽기떡볶이</Col>
+          <Col>엽기떡볶이</Col>
+        </Row>
+
+        {/* 리뷰 입력 줄 */}
+        <Row className='mt-5'>
+          <Col className='d-flex align-items-center'>
+            <Form.Group controlId='formFile' className='mb-3 '>
+              <Form.Label>사진을 올려주세요</Form.Label>
+              <Form.Control type='file' />
+            </Form.Group>
+          </Col>
+          <Col lg={8}>
+            <div class='form-group'>
+              <textarea
+                class='form-control'
+                id='exampleTextarea'
+                rows='10'
+                placeholder='음식은 어떠셨나요? 후기를 남겨주세요.'
+              ></textarea>
+            </div>
+          </Col>
+        </Row>
+        <Row className='mt-5'>
+          <p>별점을 매겨주세요</p>
+          <p className='text-warning'>
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+          </p>
+        </Row>
+
+        {/* 리뷰등록버튼 */}
+        <Row className='d-inline-flex mt-5'>
+          <Button size='lg' variant='outline-secondary'>
+            리뷰등록
+          </Button>{' '}
+        </Row>
+      </Container>
+    </ReviewWritinghWrapper>
+  );
 };
 
 export default ReviewWriting;
