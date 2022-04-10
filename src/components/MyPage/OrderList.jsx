a048fd738e69c104e7195cb0a3e2ed93b8df8733
hyoyoungOH <lisa4397@gmail.com>
2022-04-10 20:56:15 +0900

    주문목록페이지- 퍼블완료

--- src/components/MyPage/OrderList.jsx
+++ src/components/MyPage/OrderList.jsx
@@ -1,95 +1,107 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 
-const StyleTop = styled.section`
+const OrderListWrapper = styled.div`
   position: absolute;
-  left: 45%;
+  align-items: center;
+  width: 100%;
   top: 20%;
 `;
-const StyleImg = styled.img`
-  height: 10rem;
-  object-fit: cover;
-`;
-
-const StyleSection = styled.section`
-  position: relative;
-  right: 40%;
-  /* left: 50%; */
-`;
-
-const StyleTh = styled.th``;
 
 const OrderList = () => {
   return (
-    <>
-      <StyleTop className="container">
-        <h1>OrderList</h1>
-        <StyleSection>
-          <table class="table table-hover ">
-            <thead className="fs-1 ">
-              <tr>
-                <th scope="col"></th>
-                <th scope="col">store</th>
-                <th scope="col">menu</th>
-                <th scope="col">price</th>
-                <th scope="col">order date</th>
-                <th scope="col">waiting </th>
-                <th scope="col">Column heading</th>
-              </tr>
-            </thead>
-            <tbody className="fs-5 text-center ">
-              <tr>
-                <td>
-                  <Link type="img" to="/">
-                    <StyleImg src="img/food1.jpg" alt="food" />
-                  </Link>
-                </td>
-                <td className="">졸리다</td>
-                <StyleTh>살려줘</StyleTh>
-                <td>5억만</td>
-                <td>2022.4.10</td>
-                <td>50</td>
-                <td>
-                  <div>
-                    <Link
-                      type="button"
-                      className="btn btn-primary btn-lg"
-                      to="/reviewwriting"
-                    >
-                      리뷰쓰기
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <Link type="img" to="/">
-                    <StyleImg src="img/food1.jpg" alt="food" />
-                  </Link>
-                </td>
-                <td className="">졸리다</td>
-                <StyleTh>살려줘</StyleTh>
-                <td>5억만</td>
-                <td>2022.4.10</td>
-                <td>50</td>
-                <td>
-                  <div>
-                    <Link
-                      type="button"
-                      className="btn btn-primary btn-lg"
-                      to="/reviewwriting"
-                    >
-                      리뷰쓰기
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </StyleSection>
-      </StyleTop>
-    </>
+    <OrderListWrapper>
+      <Container className='text-center'>
+        <h1>주문내역</h1>
+
+        <Row className='mt-5 d-flex justify-content-center'>
+          <Col />
+          <Col className='TitleStoreName'>
+            <h4>매장명</h4>
+          </Col>
+          <Col className='TitleMenuName'>
+            <h4>메뉴명</h4>
+          </Col>
+          <Col className='TitlePrice'>
+            <h4>가격</h4>
+          </Col>
+          <Col className='TitleDate'>
+            <h4>주문일</h4>
+          </Col>
+          <Col className='TitleWaiting'>
+            <h4>대기번호</h4>
+          </Col>
+          <Col />
+        </Row>
+        <hr />
+
+        <Row className='d-flex align-items-center mb-5'>
+          <Col className='MenuImg'>
+            <Card style={{ width: '10rem', height: '10rem' }}>
+              <Card.Img
+                style={{ width: '9rem', height: '9rem' }}
+                variant='top'
+                src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Gimbap_%28pixabay%29.jpg/1280px-Gimbap_%28pixabay%29.jpg'
+              />
+            </Card>
+          </Col>
+          <Col className='StoreName'>
+            <p>롯데리아</p>
+          </Col>
+          <Col className='MenuName'>
+            <p>불고기버거</p>
+          </Col>
+          <Col className='Price'>
+            <p>21,000</p>
+          </Col>
+          <Col className='Date'>
+            <p>2020.2.2</p>
+          </Col>
+          <Col className='Waiting'>
+            <p>12번</p>
+          </Col>
+          <Col className='ReviewBtn'>
+            <Link to='/review-writing'>
+              <Button variant='outline-secondary'>리뷰쓰기</Button>{' '}
+            </Link>
+          </Col>
+        </Row>
+
+        <Row className='d-flex align-items-center'>
+          <Col className='MenuImg'>
+            <Card style={{ width: '10rem', height: '10rem' }}>
+              <Card.Img
+                style={{ width: '9rem', height: '9rem' }}
+                variant='top'
+                src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Gimbap_%28pixabay%29.jpg/1280px-Gimbap_%28pixabay%29.jpg'
+              />
+            </Card>
+          </Col>
+          <Col className='StoreName'>
+            <p>롯데리아</p>
+          </Col>
+          <Col className='MenuName'>
+            <p>불고기버거</p>
+          </Col>
+          <Col className='Price'>
+            <p>21,000</p>
+          </Col>
+          <Col className='Date'>
+            <p>2020.2.2</p>
+          </Col>
+          <Col className='Waiting'>
+            <p>12번</p>
+          </Col>
+          <Col className='ReviewBtn'>
+            <Link to='/review-writing'>
+              <Button variant='outline-secondary'>리뷰쓰기</Button>{' '}
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </OrderListWrapper>
   );
 };
 
--- src/components/MyPage/ReviewHistory.jsx
+++ src/components/MyPage/ReviewHistory.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const ReviewHistory = () => {
   return <div>리뷰 관리 페이지 입니다</div>;
