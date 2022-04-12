38e7cc87f203c4e89a1430390d63849c34a8293d
JaeHoOn Lee <selswlg@naver.com>
2022-04-12 17:41:00 +0900

    review writing 복구

--- src/app.jsx
+++ src/app.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './components/MainPage/Home';
-import Layout from './components/Layout/Layout';
-import NotFound from './components/NotFound/NotFound';
-import Login from './components/LoginPage/Login';
-import ProFile from './components/ProFilePage/ProFile';
-import Cart from './components/CartPage/Cart';
-import Store from './components/StorePage/Store';
+import React, { useState, useRef } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/MainPage/Home";
+import Layout from "./components/Layout/Layout";
+import NotFound from "./components/NotFound/NotFound";
+import Login from "./components/LoginPage/Login";
+import ProFile from "./components/ProFilePage/ProFile";
+import Cart from "./components/CartPage/Cart";
+import Store from "./components/StorePage/Store";
 
-import OrderList from './components/MyPage/OrderList';
-import ReviewHistory from './components/MyPage/ReviewHistory';
-import ReviewWriting from './components/MyPage/ReviewWriting';
-import UserInfoConfig from './components/MyPage/UserInfoConfig';
-import MemberRegister from './components/RegisterPage/MemberRegister';
-import OwnerRegister from './components/RegisterPage/OwnerRegister';
-import 'bootswatch/dist/sketchy/bootstrap.min.css';
+import OrderList from "./components/MyPage/OrderList";
+import ReviewHistory from "./components/MyPage/ReviewHistory";
+import ReviewWriting from "./components/MyPage/ReviewWriting";
+import UserInfoConfig from "./components/MyPage/UserInfoConfig";
+import MemberRegister from "./components/RegisterPage/MemberRegister";
+import OwnerRegister from "./components/RegisterPage/OwnerRegister";
+import "bootswatch/dist/sketchy/bootstrap.min.css";
 
 function App() {
   return (
     <Routes>
-      <Route path='/profile' element={<ProFile />} />
+      <Route path="/profile" element={<ProFile />} />
       <Route element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='/store' element={<Store />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/order-list' element={<OrderList />} />
-        <Route path='/review-history' element={<ReviewHistory />} />
-        <Route path='/review-writing' element={<ReviewWriting />} />
-        <Route path='/userinfo-config' element={<UserInfoConfig />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/member-register' element={<MemberRegister />} />
-        <Route path='/owner-register' element={<OwnerRegister />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order-list" element={<OrderList />} />
+        <Route path="/review-history" element={<ReviewHistory />} />
+        <Route path="/review-writing" element={<ReviewWriting />} />
+        <Route path="/userinfo-config" element={<UserInfoConfig />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/member-register" element={<MemberRegister />} />
+        <Route path="/owner-register" element={<OwnerRegister />} />
       </Route>
-      <Route path='*' element={<NotFound />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
--- src/components/MyPage/ReviewWriting.jsx
+++ src/components/MyPage/ReviewWriting.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import { AiFillStar } from 'react-icons/ai';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import React, { useState, useRef } from "react";
+import styled from "styled-components";
+import { AiFillStar } from "react-icons/ai";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import Rating from "./star/Rating";
 
 const ReviewWritinghWrapper = styled.div`
   position: absolute;
@@ -12,11 +13,51 @@ const ReviewWritinghWrapper = styled.div`
 `;
 
 const ReviewWriting = () => {
+  const [state, setState] = useState({
+    content: "",
+    img_URL: "",
+    rating: 0,
+  });
+  const contnetInput = useRef();
+  const imgURLInput = useRef();
+
+  const handleContentInput = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleImgInput = (e) => {
+    const url = e.target.files[0].name;
+    console.log(url);
+    setState({
+      ...state,
+      [e.target.name]: url,
+    });
+  };
+
+  const getRating = (score) => {
+    setState({
+      ...state,
+      rating: score,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (state.content.length < 1) {
+      contnetInput.current.focus();
+    } else if (!state.img_URL) {
+      imgURLInput.current.focus();
+    } else {
+      console.log(state);
+    }
+  };
+
   return (
     <ReviewWritinghWrapper>
-      <Container className='text-center'>
+      <Container className="text-center">
         <h1>리뷰 작성하기</h1>
-        <Row className='mt-5'>
+        <Row className="mt-5">
           <Col>
             <h3>매장명</h3>
           </Col>
@@ -25,46 +66,55 @@ const ReviewWriting = () => {
           </Col>
         </Row>
         <hr />
-        <Row className='mb-5'>
+        <Row className="mb-5">
           <Col>동대문엽기떡볶이</Col>
           <Col>엽기떡볶이</Col>
         </Row>
 
         {/* 리뷰 입력 줄 */}
-        <Row className='mt-5'>
-          <Col className='d-flex align-items-center'>
-            <Form.Group controlId='formFile' className='mb-3 '>
+        <Row className="mt-5">
+          <Col className="d-flex align-items-center">
+            <Form.Group controlId="formFile" className="mb-3 ">
               <Form.Label>사진을 올려주세요</Form.Label>
-              <Form.Control type='file' />
+              <Form.Control
+                type="file"
+                ref={imgURLInput}
+                name="img_URL"
+                onChange={handleImgInput}
+              />
             </Form.Group>
           </Col>
           <Col lg={8}>
-            <div class='form-group'>
+            <div class="form-group">
               <textarea
-                class='form-control'
-                id='exampleTextarea'
-                rows='10'
-                placeholder='음식은 어떠셨나요? 후기를 남겨주세요.'
+                name="content"
+                class="form-control"
+                id="exampleTextarea"
+                rows="10"
+                placeholder="음식은 어떠셨나요? 후기를 남겨주세요."
+                ref={contnetInput}
+                onChange={handleContentInput}
               ></textarea>
             </div>
           </Col>
         </Row>
-        <Row className='mt-5'>
+        <Row className="mt-5">
           <p>별점을 매겨주세요</p>
-          <p className='text-warning'>
+          <p className="text-warning">
+            {/* <AiFillStar />
             <AiFillStar />
             <AiFillStar />
             <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
+            <AiFillStar /> */}
+            <Rating getRating={getRating} />
           </p>
         </Row>
 
         {/* 리뷰등록버튼 */}
-        <Row className='d-inline-flex mt-5'>
-          <Button size='lg' variant='outline-secondary'>
+        <Row className="d-inline-flex mt-5">
+          <Button size="lg" variant="outline-secondary" onClick={handleSubmit}>
             리뷰등록
-          </Button>{' '}
+          </Button>{" "}
         </Row>
       </Container>
     </ReviewWritinghWrapper>
