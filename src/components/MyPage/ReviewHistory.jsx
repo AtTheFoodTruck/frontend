b985579d23ae6e1ab110a877a55bc3ea380f4e4e
JaeHoOn Lee <selswlg@naver.com>
2022-04-13 00:25:11 +0900

    review_list 표시

--- src/components/MyPage/ReviewHistory.jsx
+++ src/components/MyPage/ReviewHistory.jsx
@@ -1,6 +1,9 @@
-import { Card, Container, Button, Col, Row } from 'react-bootstrap';
-import styled from 'styled-components';
-
+import { Card, Container, Button, Col, Row } from "react-bootstrap";
+import styled from "styled-components";
+import axios from "axios";
+import React, { useState, useEffect, useRef } from "react";
+import Star from "./star";
+import { FaStar } from "react-icons/fa";
 const ReviewHistoryWrapper = styled.div`
   position: absolute;
   align-items: center;
@@ -10,95 +13,161 @@ const ReviewHistoryWrapper = styled.div`
 `;
 
 const ReviewHistory = () => {
+  const [test, setTest] = useState([]);
+
+  const test2 = [
+    {
+      postId: 1,
+      id: 1,
+      name: "피자",
+      price: 12000,
+      email: "ada@gardner.biz",
+      body: "lasi\nreiciendis et nam sapiente accusantium",
+      rating: 3,
+    },
+    {
+      postId: 2,
+      id: 2,
+      name: "햄버거",
+      price: 8000,
+      email: "Eliseo@gardner.biz",
+      body: "laudantium enim quasi est quidem magnam voluptateapiente accusantium",
+      rating: 4,
+    },
+    {
+      postId: 3,
+      id: 3,
+      name: "핫도그",
+      price: 4000,
+      email: "Eliseo@gardner.biz",
+      body: "laudantium enim quasi est quidem magnam voluptateapiente accusantium",
+      rating: 5,
+    },
+
+    {
+      postId: 3,
+      id: 3,
+      name: "핫도그",
+      price: 4000,
+      email: "Eliseo@gardner.biz",
+      body: "laudantium enim quasi est quidem magnam voluptateapiente accusantium",
+      rating: 4,
+    },
+    {
+      postId: 3,
+      id: 3,
+      name: "핫도그",
+      price: 4000,
+      email: "Eliseo@gardner.biz",
+      body: "laudantium enim quasi est quidem magnam voluptateapiente accusantium",
+      rating: 3,
+    },
+  ];
+
+  const testList = () => {
+    axios.get("https://jsonplaceholder.typicode.com/comments/").then((res) => {
+      setTest(res.data);
+    });
+  };
+
+  useEffect(() => {
+    testList();
+  }, []);
+
+  //img
+  //음식이름
+  //가격
+  //별점 rating
+  //삭제
+  //리뷰 내용
+
   return (
     <ReviewHistoryWrapper>
-      <Container className='ReviewPage text-center mt-5'>
+      <Container className="ReviewPage text-center mt-5">
         <h1>리뷰관리</h1>
+        {/* 이미지 */}
+        {test2.map((it, idx) => (
+          <Row
+            className="mt-5 mb-5 d-flex align-items-center justify-content-center"
+            key={idx}
+          >
+            <Col className="d-flex justify-content-center">
+              <Card style={{ width: "8rem", height: "8rem" }}>
+                <Card.Img
+                  variant="top"
+                  src="https://mp-seoul-image-production-s3.mangoplate.com/999285_1575800181007324.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80"
+                />
+              </Card>
+            </Col>
 
-        {/*리뷰 리스트 */}
-        {/* 음식이미지 */}
-        <Row className='mt-5 mb-5 d-flex align-items-center justify-content-center'>
-          <Col className='d-flex justify-content-center'>
-            <Card style={{ width: '8rem', height: '8rem' }}>
-              <Card.Img
-                variant='top'
-                src='https://mp-seoul-image-production-s3.mangoplate.com/999285_1575800181007324.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80'
-              />
-            </Card>
-          </Col>
+            {/* 항목 */}
+            <Col lg={9}>
+              <Row>
+                <Col className="MenuName">
+                  <h5>{it.name}</h5>
+                </Col>
+                <Col className="Price">
+                  <h5>{it.price}</h5>
+                </Col>
+                <Col className="Rating text-warning">
+                  <Star rating={it.rating} />
+                </Col>
+                <Col className="Date">
+                  <h5>2222.22.22</h5>
+                </Col>
+                <Col className="deleteBtn">
+                  <Button variant="outline-secondary">삭제</Button>
+                </Col>
+              </Row>
 
-          {/* 항목 */}
-          <Col lg={9}>
-            <Row>
-              <Col className='MenuName'>
-                <h5>햄버거</h5>
-              </Col>
-              <Col className='Price'>
-                <h5>10,000</h5>
-              </Col>
-              <Col className='Rating text-warning'>
-                <i class='fa fa-star'></i>
-                <i class='fa fa-star'></i>
-                <i class='fa fa-star'></i>
-                <i class='fa fa-star'></i>
-                <i class='fa fa-star'></i>
-              </Col>
-              <Col className='Date'>
-                <h5>2222.22.22</h5>
-              </Col>
-              <Col className='deleteBtn'>
-                <Button variant='outline-secondary'>삭제</Button>
-              </Col>
-            </Row>
-
-            {/* 리뷰텍스트 */}
-            <Row className='mt-3'>
-              <Card.Body>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-            </Row>
-          </Col>
-          <hr className='mt-5' />
-        </Row>
-
-        {/*페이징 처리*/}
-        <div className='d-flex justify-content-center'>
-          <ul class='pagination'>
-            <li class='page-item disabled'>
-              <a class='page-link' href='#'>
+              {/* 리뷰텍스트 */}
+              <Row className="mt-3">
+                <Card.Body>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Row>
+            </Col>
+            <hr className="mt-5" />
+          </Row>
+        ))}
+        ;{/*페이징 처리*/}
+        <div className="d-flex justify-content-center">
+          <ul class="pagination">
+            <li class="page-item disabled">
+              <a class="page-link" href="#">
                 &laquo;
               </a>
             </li>
-            <li class='page-item active'>
-              <a class='page-link' href='#'>
+            <li class="page-item active">
+              <a class="page-link" href="#">
                 1
               </a>
             </li>
-            <li class='page-item'>
-              <a class='page-link' href='#'>
+            <li class="page-item">
+              <a class="page-link" href="#">
                 2
               </a>
             </li>
-            <li class='page-item'>
-              <a class='page-link' href='#'>
+            <li class="page-item">
+              <a class="page-link" href="#">
                 3
               </a>
             </li>
-            <li class='page-item'>
-              <a class='page-link' href='#'>
+            <li class="page-item">
+              <a class="page-link" href="#">
                 4
               </a>
             </li>
-            <li class='page-item'>
-              <a class='page-link' href='#'>
+            <li class="page-item">
+              <a class="page-link" href="#">
                 5
               </a>
             </li>
-            <li class='page-item'>
-              <a class='page-link' href='#'>
+            <li class="page-item">
+              <a class="page-link" href="#">
                 &raquo;
               </a>
             </li>
