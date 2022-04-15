bf4706d4b0ada3642e47e643dfafa5da38e566c1
Goalgoru <47812147+Goalgoru@users.noreply.github.com>
2022-04-15 15:28:01 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
->>>>>>> origin/feature-searchlist
+
 
 ReactDOM.render(
   <BrowserRouter>
