d58e2747b5a85e748ab6458599b09b04927a600a
goalgoru <mjforwarder7411@gmail.com>
2022-04-20 02:37:46 +0900

    로그인 UI

--- src/components/LoginPage/LoginInput.jsx
+++ src/components/LoginPage/LoginInput.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
 
 const InputContainer = styled.form`
   .email {
@@ -18,11 +18,11 @@ const InputContainer = styled.form`
 `;
 
 const LoginInput = () => {
-  const [inputId, setInputId] = useState('');
-  const [inputPw, setInputPw] = useState('');
-  let [accessToken, setAccessToken] = useState('');
-  let [refreshToken, setRefreshToken] = useState('');
-  let [userId, setUserId] = useState('');
+  const [inputId, setInputId] = useState("");
+  const [inputPw, setInputPw] = useState("");
+  let [accessToken, setAccessToken] = useState("");
+  let [refreshToken, setRefreshToken] = useState("");
+  let [userId, setUserId] = useState("");
   const navigate = useNavigate();
 
   const handleInputPw = (e) => {
@@ -36,31 +36,28 @@ const LoginInput = () => {
   };
 
   useEffect(() => {
-    localStorage.setItem('Authorization', accessToken);
-    localStorage.setItem('userId', userId);
-    localStorage.setItem('refreshToken', refreshToken);
-    console.log('accessToken : ' + accessToken);
-    console.log('userId : ' + userId);
-    console.log('refreshToken : ' + refreshToken);
+    localStorage.setItem("Authorization", accessToken);
+    localStorage.setItem("userId", userId);
+    localStorage.setItem("refreshToken", refreshToken);
+    console.log("accessToken : " + accessToken);
+    console.log("userId : " + userId);
+    console.log("refreshToken : " + refreshToken);
   }, [accessToken, userId, refreshToken]);
 
   async function onClickLogin(e) {
     e.preventDefault();
 
-    if (inputId === '') {
-      alert('아이디를 입력하세요');
-    } else if (inputPw === '') {
-      alert('비밀번호를 입력하세요');
+    if (inputId === "") {
+      alert("아이디를 입력하세요");
+    } else if (inputPw === "") {
+      alert("비밀번호를 입력하세요");
     } else {
       axios
-        .post('http://localhost:8000/user-service/users/v1/join', {
+        .post("http://localhost:8000/user-service/users/v1/login", {
           email: inputId,
           password: inputPw,
         })
         .then(function (response) {
-          console.log(response.data.accesToken);
-          console.log(response.data.refreshToken);
-          console.log(response.data.userId);
           setAccessToken(response.data.accesToken);
           setRefreshToken(response.data.refreshToken);
           setUserId(response.data.userId);
@@ -71,39 +68,41 @@ const LoginInput = () => {
 
   return (
     <InputContainer>
-      <div className="login__body--id">
-        <label htmlFor="input_id" className="login__body--id-icon">
-          {' '}
-        </label>
-        <input
-          type="text"
-          name="input_id"
-          value={inputId}
-          className="login__body--id-input"
-          id="input_id"
-          placeholder="아이디를 입력하세요."
-          onChange={handleInputId}
-        />
+      <h1>Login</h1>
+
+      <div className="email">
+        <div className="form-floating">
+          <input
+            type="text"
+            name="input_id"
+            value={inputId}
+            className="form-control"
+            id="input_id"
+            placeholder="아이디를 입력하세요."
+            onChange={handleInputId}
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
       </div>
 
-      <div className="login__body--pw">
-        <label htmlFor="input_pw" className="login__body--pw-icon">
-          {' '}
-        </label>
-        <input
-          type="password"
-          name="input_pw"
-          value={inputPw}
-          className="login__body--pw-input"
-          id="input_pw"
-          placeholder="비밀번호를 입력하세요."
-          onChange={handleInputPw}
-        />
+      <div className="password">
+        <div className="form-floating">
+          <input
+            type="password"
+            name="input_pw"
+            value={inputPw}
+            className="form-control"
+            id="input_pw"
+            placeholder="비밀번호를 입력하세요."
+            onChange={handleInputPw}
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
       </div>
 
       <button
         type="submit"
-        className="login__btn--signin"
+        className="btn btn-lg btn-outline-secondary"
         onClick={onClickLogin}
       >
         로그인
