41e3bb0b54dd5383b6e49c85e63595dc582fa054
jaemin <fkdlem524@naver.com>
2022-04-20 17:57:42 +0900

    이재민 - 카트목록조회 요청 함수 생성

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -1,4 +1,4 @@
-import { React, useRef, useState } from 'react';
+import { React, useEffect, useRef, useState } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import styled from 'styled-components';
 import CartList from './CartList';
@@ -9,12 +9,22 @@ import axios from 'axios';
 // })
 // .then(function ())
 
-const handleClick = () => alert(`주문 완료되었습니다!`);
+// const handleClick = () => alert(`주문 완료되었습니다!`);
 
 const Cart = () => {
+  // 변수 선언부
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const accessToken = localStorage.getItem('Authorization');
+  let cartArray = [];
 
+  // headers
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  // 데이터 조작 시작
   const onAdd = (store_name, menu, image, price) => {
     const newItem = {
       store_name,
@@ -27,34 +37,94 @@ const Cart = () => {
     setData([newItem, ...data]);
   };
 
+  // 아이템 삭제 버튼 클릭 이벤트
   const onRemove = (targetId) => {
     const newCartList = data.filter((list) => list.id !== targetId);
     setData(newCartList);
   };
 
-  const [totalPrice, setTotalPrice] = useState(0);
-
+  // 총 가격
   const handTotalPrice = (price) => setTotalPrice(totalPrice + price);
+
+  // 최초 브라우저 rendering되었을 때 실행
+  useEffect(() => {
+    const request = `http://localhost:8000/order-service/orders/v1/customer/carts`;
+    printCartList(request);
+
+    /* 이재민 샘플 코드
+    `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=`;
+    callMovie(response);
+    getMovie(response);
+    */
+  }, []);
+
+  /* 이재민 샘플 코드
+  const callMovie = async (response) => {
+    let movieList = [];
+    for (let i = 1; i < 11; i++) {
+      movieList = movieList.concat(
+        await fetch(response + i)
+          .then((response) => response.json())
+          .then((response) => response.results)
+      );
+    }
+    setMovies(movieList.concat());
+    setMainMovieImage(movieList[0]);
+  };
+  */
+  const printCartList = async (request) => {
+    axios
+      .get(
+        request, // 카트목록조회 URL
+        //'http://localhost:8000/order-service/orders/v1/customer/carts',
+        {
+          // body
+          user_id: 1,
+        },
+        {
+          headers: headers,
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        // return 받은 데이터를 newCart 객체로 담기
+        const newCart = {
+          orderItemId: response.data.data.orderItemId,
+          storeName: response.data.data.storeName,
+          itemId: response.data.data.itemId,
+          itemName: response.data.data.itemName,
+          itemImgUrl: response.data.data.itemImgUrl,
+          count: response.data.data.count,
+          totalPrice: response.data.data.totalPrice,
+        };
+
+        // 위에서 선언한 cartArray에 newCart를 담기, cartArray에 담긴 객체를 화면에 출력해줘야함!!!!
+        cartArray.push(newCart);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <CartWrapper>
-      <Container className='mt-5'>
-        <h1 className='text-center'>Cart</h1>
+      <Container className="mt-5">
+        <h1 className="text-center">Cart</h1>
         <h4>{data.store_name}</h4>
-        <Row className='d-flex justify-content-evenly mt-5'>
+        <Row className="d-flex justify-content-evenly mt-5">
           <Col lg={5}></Col>
-          <Col className='d-flex justify-content-center p-0 ms-5 me-4'>
+          <Col className="d-flex justify-content-center p-0 ms-5 me-4">
             <h5>Menu</h5>
           </Col>
-          <Col className='d-flex justify-content-center p-0'>
+          <Col className="d-flex justify-content-center p-0">
             <h5>Amout</h5>
           </Col>
-          <Col className='d-flex justify-content-center p-0 me-3'>
+          <Col className="d-flex justify-content-center p-0 me-3">
             <h5>Price</h5>
           </Col>
         </Row>
 
-        <Row className='StoreName d-flex justify-content-start mt-5'>
+        <Row className="StoreName d-flex justify-content-start mt-5">
           <hr />
         </Row>
 
@@ -65,18 +135,18 @@ const Cart = () => {
             onRemove={onRemove}
             handTotalPrice={handTotalPrice}
           />
-          <Row className='text-end mt-5'>
+          <Row className="text-end mt-5">
             <h4>총 금액 : {totalPrice.toLocaleString()}</h4>
           </Row>
         </Row>
 
         {/* 주문하기 버튼 */}
-        <Container className='text-center'>
+        <Container className="text-center">
           <Button
             onClick={handleClick}
-            className='mt-5'
-            size='lg'
-            variant='outline-secondary'
+            className="mt-5"
+            size="lg"
+            variant="outline-secondary"
           >
             주문하기
           </Button>{' '}
