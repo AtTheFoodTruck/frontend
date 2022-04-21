90c16dc0089068bba906bd6221e8406c230ee09d
hyoyoungOH <lisa4397@gmail.com>
2022-04-21 20:41:34 +0900

    카트- 주문생성 api

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -89,7 +89,7 @@ const Cart = () => {
   //주문생성 및 주문 완료 버튼
   async function handleClick() {
     axios.post(
-      'http://localhost:8000/order-service/orders/v1/customer/order',
+      'https://apifood.blacksloop.com/order-service/orders/v1/customer/order',
       {
         //body
         user_id: 1,
