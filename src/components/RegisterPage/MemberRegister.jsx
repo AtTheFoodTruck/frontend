ff8571fe9145d49f4d89dbf711bbef86c95dc3dd
jaemin <fkdlem524@naver.com>
2022-04-21 17:32:10 +0900

    uri 서비스명

--- src/components/RegisterPage/MemberRegister.jsx
+++ src/components/RegisterPage/MemberRegister.jsx
@@ -69,7 +69,7 @@ const MemberRegister = () => {
       alert('비밀번호를 입력하세요');
     } else {
       axios
-        .post('http://gateway/user-service/users/v1/join', {
+        .post('http://api-gateway/user-service/users/v1/join', {
           email: inputEmail,
           username: inputNickname,
           password: inputPw,
