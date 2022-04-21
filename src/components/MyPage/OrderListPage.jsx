c83bbbe341280355a56b5720d53edfb77a563f70
hyoyoungOH <lisa4397@gmail.com>
2022-04-21 20:13:42 +0900

    api test

--- src/components/MyPage/OrderListPage.jsx
+++ src/components/MyPage/OrderListPage.jsx
@@ -2,83 +2,76 @@ import React, { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import { Container, Row, Col } from 'react-bootstrap';
 import OrderList from './OrderList';
-
-// const DUMMYDATA = [
-//   {
-//     id: 1,
-//     store_name: '엽기떡볶이',
-//     menu: '엽기떡볶이',
-//     price: 5000,
-//     ordered_date: '2022.02.12',
-//     waiting_num: 13,
-//     menu_img: 'https://via.placeholder.com/150/24f355',
-//   },
-//   {
-//     id: 2,
-//     store_name: '맛나분식',
-//     menu: '김밥',
-//     price: 4000,
-//     ordered_date: '2022.11.12',
-//     waiting_num: 15,
-//     menu_img: 'https://via.placeholder.com/150/24f355',
-//   },
-//   {
-//     id: 3,
-//     store_name: '천재치킨',
-//     menu: '양념치킨',
-//     price: 23000,
-//     ordered_date: '2022.02.10',
-//     waiting_num: 3,
-//     menu_img: 'https://via.placeholder.com/150/24f355',
-//   },
-//   {
-//     id: 4,
-//     store_name: '쉐프의 스테이크',
-//     menu: '오늘의 스테이크',
-//     price: 12200,
-//     ordered_date: '2022.02.03',
-//     waiting_num: 23,
-//     menu_img: 'https://via.placeholder.com/150/24f355',
-//   },
-//   {
-//     id: 5,
-//     store_name: '밥버거',
-//     menu: '제육밥버거',
-//     price: 5200,
-//     ordered_date: '2012.02.22',
-//     waiting_num: 16,
-//     menu_img: 'https://via.placeholder.com/150/24f355',
-//   },
-// ];
-
-// https://jsonplaceholder.typicode.com/photos
+import axios from 'axios';
 
 const OrderListPage = () => {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
 
-  const getData = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/photos').then(
-      (res) => res.json()
-    );
+  // const headers = {
+  //   Authorization: `Bearer ${accessToken}`,
+  // };
 
-    const initData = res.slice(0, 20).map((list) => {
-      return {
-        store_name: list.title,
-        menu: list.title,
-        price: 5000,
-        ordered_date: '2022.12.23',
-        waiting_num: list.albumId,
-        menu_img: list.thumbnailUrl,
-        id: dataId.current++,
-      };
-    });
-    setData(initData);
-  };
+  // const getData = async () => {
+  //   const res = await fetch(
+  //     'https://apifood.blacksloop.com/orders/v1/customer/order'
+  //   ).then((res) => res.json());
+
+  //   const initData = res.slice(0, 20).map((list) => {
+  //     return {
+  //       store_name: list.title,
+  //       menu: list.title,
+  //       price: 5000,
+  //       ordered_date: '2022.12.23',
+  //       waiting_num: list.albumId,
+  //       menu_img: list.thumbnailUrl,
+  //       id: dataId.current++,
+  //     };
+  //   });
+  //   setData(initData);
+  // };
 
   useEffect(() => {
-    getData();
+    axios
+      .get('https://apifood.blacksloop.com/orders/v1/customer/order')
+      .then(function (response) {
+        console.log(response);
+      });
   }, []);
+
+  // const getData = async (request) => {
+  //   axios
+  //     .get(
+  //       request, // 카트목록조회 URL
+  //       //'http://localhost:8000/order-service/orders/v1/customer/carts',
+  //       {
+  //         // body
+  //         user_id: 1,
+  //       },
+  //       {
+  //         headers: headers,
+  //       }
+  //     )
+  //     .then((response) => {
+  //       console.log(response);
+  //       // return 받은 데이터를 newCart 객체로 담기
+  //       const newCart = {
+  //         orderItemId: response.data.data.orderItemId,
+  //         storeName: response.data.data.storeName,
+  //         itemId: response.data.data.itemId,
+  //         itemName: response.data.data.itemName,
+  //         itemImgUrl: response.data.data.itemImgUrl,
+  //         count: response.data.data.count,
+  //         totalPrice: response.data.data.totalPrice,
+  //       };
+
+  // 위에서 선언한 cartArray에 newCart를 담기, cartArray에 담긴 객체를 화면에 출력해줘야함!!!!
+  //       cartArray.push(newCart);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // };
 
   return (
     <OrderListWrapper>
