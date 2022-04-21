e4cc55d3ee5c17740b01cf999f95f9634ad3ade0
hyoyoungOH <lisa4397@gmail.com>
2022-04-21 16:01:36 +0900

    장바구니 주문 생성

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -106,25 +106,41 @@ const Cart = () => {
       });
   };
 
+  //주문생성 및 주문 완료 버튼
+  async function handleClick() {
+    axios.post(
+      'http://localhost:8000/order-service/orders/v1/customer/order',
+      {
+        //body
+        user_id: 1,
+        //
+      },
+      {
+        headers: headers,
+      }
+    );
+    alert(`주문 완료되었습니다!`);
+  }
+
   return (
     <CartWrapper>
-      <Container className="mt-5">
-        <h1 className="text-center">Cart</h1>
+      <Container className='mt-5'>
+        <h1 className='text-center'>Cart</h1>
         <h4>{data.store_name}</h4>
-        <Row className="d-flex justify-content-evenly mt-5">
+        <Row className='d-flex justify-content-evenly mt-5'>
           <Col lg={5}></Col>
-          <Col className="d-flex justify-content-center p-0 ms-5 me-4">
+          <Col className='d-flex justify-content-center p-0 ms-5 me-4'>
             <h5>Menu</h5>
           </Col>
-          <Col className="d-flex justify-content-center p-0">
+          <Col className='d-flex justify-content-center p-0'>
             <h5>Amout</h5>
           </Col>
-          <Col className="d-flex justify-content-center p-0 me-3">
+          <Col className='d-flex justify-content-center p-0 me-3'>
             <h5>Price</h5>
           </Col>
         </Row>
 
-        <Row className="StoreName d-flex justify-content-start mt-5">
+        <Row className='StoreName d-flex justify-content-start mt-5'>
           <hr />
         </Row>
 
@@ -135,18 +151,18 @@ const Cart = () => {
             onRemove={onRemove}
             handTotalPrice={handTotalPrice}
           />
-          <Row className="text-end mt-5">
+          <Row className='text-end mt-5'>
             <h4>총 금액 : {totalPrice.toLocaleString()}</h4>
           </Row>
         </Row>
 
         {/* 주문하기 버튼 */}
-        <Container className="text-center">
+        <Container className='text-center'>
           <Button
             onClick={handleClick}
-            className="mt-5"
-            size="lg"
-            variant="outline-secondary"
+            className='mt-5'
+            size='lg'
+            variant='outline-secondary'
           >
             주문하기
           </Button>{' '}
