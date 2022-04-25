c48cb41070cbf37f35ea79fe6cd21a62053851d0
hyoyoungOH <lisa4397@gmail.com>
2022-04-25 17:52:56 +0900

    카트

--- src/components/CartPage/CartList.jsx
+++ src/components/CartPage/CartList.jsx
@@ -1,16 +1,16 @@
 import CartListItem from './CartListItem';
 
 // Q. 받아와야 할 장바구니 아이템 데이터: 메뉴이미지, 메뉴 단가, 메뉴명
-const CartList = ({ cartList, handTotalPrice, onRemove }) => {
-  console.log('cartList :' + cartList);
+const CartList = ({ cartlists, handTotalPrice, onRemove }) => {
+  console.log('cartList :' + cartlists);
   console.log('handTotalPrice ' + handTotalPrice);
 
   return (
     <span className='list'>
-      {cartList.map((cartList) => (
+      {cartlists.map((cartlists) => (
         <CartListItem
-          key={cartList.orderItemId}
-          cartList={cartList}
+          key={cartlists.orderItemId}
+          cartList={cartlists}
           // id={cartList.orderItemId}
           // image={cartList.itemImgUrl}
           // menu={cartList.itemName}
