4a4fb5e365e6f4b6e051a93b1dbe6369c0638fc8
JH_Lee <selswlhhh@gmail.com>
2022-04-27 02:34:21 +0900

    로컬호스트로 url 수정

--- src/components/SearchListPage/SearchList.jsx
+++ src/components/SearchListPage/SearchList.jsx
@@ -39,7 +39,8 @@ const SearchList = () => {
   useEffect(async () => {
     await axios
       .post(
-        `https://apifood.blacksloop.com/item-service/items/v1/search/stores?page=0&size=${size}`,
+        `http://localhost:8000/item-service/items/v1/search/stores?page=0&size=${size}`,
+        // `https://apifood.blacksloop.com/item-service/items/v1/search/stores?page=0&size=${size}`,
         {
           latitude: "1600.93", //localStorage.getItem("latitude");
           longitude: "150.156", //localStorage.getItem("longitude")
@@ -58,7 +59,8 @@ const SearchList = () => {
   useEffect(async () => {
     await axios
       .post(
-        `https://apifood.blacksloop.com/item-service/items/v1/search/stores?page=${currentPage}&size=${size}`,
+        `http://localhost:8000/item-service/items/v1/search/stores?page=0&size=${size}`,
+        // `https://apifood.blacksloop.com/item-service/items/v1/search/stores?page=${currentPage}&size=${size}`,
         {
           latitude: "1600.93", //localStorage.getItem("latitude");
           longitude: "150.156", //localStorage.getItem("longitude")
